c73b174f4917a1efa693ac2809b4372c07cdf256
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2018-10-06 05:56:32 +0530

    iter

--- t1.py
+++ t1.py
@@ -3,13 +3,13 @@ import math
 pygame.init()
 a=pygame.mixer.Sound("a3.wav")
 #ang_vel=math.pi/4
-ang_vel=math.pi/4
+ang_vel=math.pi/8
 frame_rate=60
 pygame.mixer.Channel(0).play(a, loops=-1)
 clock=pygame.time.Clock()
 theta=0
-amplitude=8
-R=14
+amplitude=80
+R=20
 r=11
 R_sq=R**2
 r_sq=r**2
@@ -17,8 +17,12 @@ tw_rr=2*(R)*(r)
 a=150
 b=100
 while True:
-    r_vol=amplitude/((R-r*math.cos(theta))**2)
-    l_vol=amplitude/((R+r*math.cos(theta))**2)
+    x2=R*math.cos(theta)
+    y2=R*math.sin(theta)
+    r_vol=amplitude/((x2-r)**2+(y2)**2)
+    l_vol=amplitude/((x2+r)**2+(y2)**2)
+    #r_vol=amplitude/((R-r*math.cos(theta))**2)
+    #l_vol=amplitude/((R+r*math.cos(theta))**2)
     #r_vol=amplitude/((R_sq + r_sq - tw_rr*math.cos(theta)))
     #l_vol=amplitude/((R_sq + r_sq + tw_rr*math.cos(theta)))
     #r_vol=amplitude/((b**2)*((math.cos(theta))**2) + (r**2) + (a*math.sin(theta))**2 - 2*b*r*math.cos(theta))
